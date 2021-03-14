a6999fdd270cba025bd0fac991c7285726b5accf
Sethmichael01 <sethmichael1.sm@gmail.com>
2021-03-14 17:41:10 +0100

    Initial Commit

---
+++ HangFIRETest/Job.cs
@@ -0,0 +1,30 @@
+﻿using HangFIRETest.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFIRETest
+{
+    public class Job
+    {
+        #region Property
+        private readonly IEmployeeService _employeeService;
+        #endregion
+
+        #region Constructor
+        public Job(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        #endregion
+
+        #region Job Scheduler
+        public async Task<bool> JobAsync()
+        {
+            var result = await _employeeService.InsertEmployeeAsync();
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ HangFIRETest/Model/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HangFIRETest.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public string Designation { get; set; }
+    }
+}
